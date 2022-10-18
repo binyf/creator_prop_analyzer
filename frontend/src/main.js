a033072d47e46d10b53f4a88ef308b3b72919751
binyf <binyf3640@gmail.com>
2022-10-18 09:27:18 +0900

    feat: front: dialog for channel selection

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,18 +8,39 @@ import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
+import Grow from '@mui/material/Grow';
+import Alert from '@mui/material/Alert'
+import Snackbar from '@mui/material/Snackbar';
+import Dialog from "@mui/material/Dialog";
+import ChannelCard from "./channelCard";
+
 
 function Main() {
   const [text, setText] = React.useState('')
   const [channels, setChannels] = React.useState([])
   const [check, setCheck] = React.useState(false)
+  const [alert, setAlert] = React.useState(false);
+  const [ch_info, setInfo] = React.useState({id:'',name:'',img:''});
+  const [open, setOpen] = React.useState(false);
+  const handleAlert = () => {
+    setAlert(true);
+  };
+
+  const handleAlertClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setAlert(false);
+  };
+
   const channel_search = (channel_name) => {
-    console.log(process.env.REACT_APP_PROXY_HOST)
-    console.log(channel_name)
+    setCheck(false)
     const requestOptions = {
       method: "GET",
       headers: {
@@ -31,16 +52,20 @@ function Main() {
     fetch(`/api/v1/channel/${channel_name}`, requestOptions)
       .then((response) => response.json())
       .then((json) => {
-        console.log(json)
         setChannels(json.channels)
-        console.log(channels)
         setCheck(true)
       })
       .catch((error) => {console.log(error)});
   };
   return (
+    <div>
+      <Snackbar open={alert} autoHideDuration={3000} onClose={handleAlertClose}>
+        <Alert variant="filled" onClose={handleAlertClose} severity="error" sx={{ width: '100%' }}>
+          채널명을 입력해주세요!
+        </Alert>
+      </Snackbar>
     <div className="outer-div">
-      <Container component='main' maxWidth="lg">
+      <Container component='main' maxWidth="xl">
         <Box
         sx={{
           marginTop: 8,
@@ -64,7 +89,7 @@ function Main() {
         <TextField 
         fullWidth 
         value = {text} 
-        label="fullWidth" 
+        label="채널 검색" 
         onChange = {(e)=>setText(e.target.value)} />
         <Button 
         size = 'large'
@@ -72,16 +97,30 @@ function Main() {
           if (text !== ''){
             channel_search(text)
           }
+          else{
+            handleAlert()
+          }
           
         }}
         >검색</Button>
         </Toolbar>
 
       {check ? 
-        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        <List sx={{ width: '100%', bgcolor: 'background.paper', overflow: 'auto', maxHeight: `${window.innerHeight*0.6}px`}}>
           {
             channels.map((channel,index) => (
-            <ListItem alignItems="flex-start">
+              <Grow in={check}  {...(check ? { timeout: 300*(index+1) } : {})}>
+            <ListItemButton 
+            alignItems="flex-start" 
+            onClick = {() =>{
+              setInfo({
+                id:channel.channel_id,
+                name:channel.channel_name,
+                img:channel.thumbnail
+              });
+              setOpen(true);
+            }
+            }>
             <ListItemAvatar>
               <Avatar src={channel.thumbnail} />
             </ListItemAvatar>
@@ -100,13 +139,20 @@ function Main() {
                 </React.Fragment>
               }
             />
-            </ListItem>
+            </ListItemButton>
+            </Grow>
             ))
           }
-          
         </List>
       :<div/>}
+      <Dialog
+          open={open}
+          onClose={()=>{setOpen(false)}}
+        >
+          <ChannelCard info = {ch_info}/>
+        </Dialog>
       </Container>
+    </div>
     </div>
   );
 }
